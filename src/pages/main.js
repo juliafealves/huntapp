ff19da70d22cfc05a1d1880323dd4828f95a2bac
juliafealves <giT#8985>
2019-05-06 12:04:19 -0300

    Rearranged files and added navigation per reaction

---
+++ src/pages/main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Main extends Component {
+  static navigationOptions = {
+    title: "JSHunt"
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>Página Main.</Text>
+      </View>
+    );
+  }
+}
+
+export default Main;
