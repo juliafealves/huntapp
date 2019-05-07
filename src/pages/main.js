74ea2c7b01815ddd7bd86da8874d89bb75075375
juliafealves <giT#8985>
2019-05-07 09:31:03 -0300

    Fetch all data of products API.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import api from "./../services/api";
 
 class Main extends Component {
   static navigationOptions = {
     title: "JSHunt"
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get("/products");
+    const { docs } = response.data;
+    console.log(docs);
   };
 
   render() {
