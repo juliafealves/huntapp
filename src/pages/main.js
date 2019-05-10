c172c59302d0515c8a60ee606b7cbfebd1faca69
juliafealves <giT#8985>
2019-05-10 10:53:42 -0300

    Styling the product list.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,47 +1,90 @@
-import React, { Component } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
-import api from "./../services/api";
+import React, { Component }                                   from "react";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
+import api                                                    from "./../services/api";
 
 class Main extends Component {
-  static navigationOptions = {
-    title: "JSHunt"
-  };
+    static navigationOptions = {
+        title: "JSHunt"
+    };
 
-  state = {
-    docs: []
-  };
+    state = {
+        docs: []
+    };
 
-  componentDidMount() {
-    this.loadProducts();
-  }
+    componentDidMount() {
+        this.loadProducts();
+    }
 
-  loadProducts = async () => {
-    const response = await api.get("/products");
-    const { docs } = response.data;
-    this.setState({ docs });
-  };
+    loadProducts = async () => {
+        const response = await api.get("/products");
+        const { docs } = response.data;
+        this.setState({ docs });
+    };
 
-  renderItem = ({ item }) => (
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.description}</Text>
-        <TouchableOpacity onPress={() => {}}>
-          <Text>Acessar</Text>
-        </TouchableOpacity>
-      </View>
-  );
-
-  render() {
-    return (
-      <View>
-        <FlatList
-            data={this.state.docs}
-            keyExtractor={item => item._id}
-            renderItem={this.renderItem}
-        />
-      </View>
+    renderItem = ({ item }) => (
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
+            <TouchableOpacity
+                style={styles.productButton}
+                onPress={() => {
+                }}>
+                <Text style={styles.productButtonText}>Acessar</Text>
+            </TouchableOpacity>
+        </View>
     );
-  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    contentContainerStyle={styles.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FAFAFA"
+    },
+    list: {
+        padding: 20
+    },
+    productContainer: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20
+    },
+    productTitle: {
+        fontSize: 16,
+        color: "#999",
+        marginTop: 5,
+        lineHeight: 24
+    },
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#DA552F",
+        backgroundColor: "transparent",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10
+    },
+    productButtonText: {
+        fontSize: 16,
+        color: "#DA552F",
+        fontWeight: "bold"
+    }
+});
 
 export default Main;
